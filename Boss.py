98cc694ef747517d0aebb9882cacf1a1fb6b55c4
Samuel Missier <samuel.missier@univ-tlse3.fr>
2023-11-24 10:48:48 +0100

    end boss_minion

--- Boss.py
+++ Boss.py
@@ -14,3 +14,19 @@ if __name__ == "__main__":
     b = Boss()
     t = Task("000")
     b.createTask()
+from Manager import QueueClient
+from Task import Task
+
+
+class Boss(QueueClient):
+    def __init__(self):
+        self.queue = QueueClient()
+
+    def createTask(self, task):
+        self.queue.tasks.put(task)
+
+
+if __name__ == "__main__":
+    b = Boss()
+    t = Task("000")
+    b.createTask(t)
